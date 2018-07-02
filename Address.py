e99e90e07e462f8c4f981a06b862844c763b62ad
Cryptic <t.tjabane@gmail.com>
2018-07-02 19:49:57 +0200

    finished the addresses functionality, I have attached the start up readme file too

--- Address.py
+++ Address.py
@@ -11,32 +11,37 @@ public_key = private_key.get_verifying_key()
 string_public_key = public_key.to_string()
 
 #Perform SHA-256 hashing on the public key
-string_public = sha256(string_public_key).hexdigest()
+sha = hashlib.sha256()
+sha.update(string_public_key)
+string_public = sha.digest()
+
 
 # Perform RIPEMD-160 hashing on the result of SHA-256
 ripemd160 = hashlib.new('ripemd160')
 ripemd160.update(string_public)
-string_public = ripemd160.hexdigest()
+sharip = ripemd160.hexdigest()
 
 
 #add version byte in front of RIPEMD-160 hash (0x00 for Main Network)
-main_address = "00" + string_public
+main_address = "00" + sharip
 
 #Perform SHA-256 hash on the extended RIPEMD-160 result twice
-address = sha256(sha256(main_address).hexdigest()).hexdigest()
+checksum = sha256(sha256(main_address.decode('hex')).digest()).hexdigest()
 
 # Take the first 4 bytes of the second SHA-256 hash. This is the address checksum
-checksum = address[:8]
-
+checksum = checksum[:8]
+print "checksum: " + checksum
 
 # Add the 4 checksum bytes from stage 7 at the end of extended RIPEMD-160 hash from stage 4. This is the 25-byte binary Bitcoin Address.
-main_address = main_address + checksum
+main_address = main_address + checksum.upper()
 
 #Convert the result from a byte string into a base58 string using Base58Check encoding. This is the most commonly used Bitcoin Address format
-unencoded_string = str(bytearray.fromhex(main_address))
-main_address = base58.b58encode(unencoded_string)
+main_address = base58.b58encode(main_address.decode('hex'))
 
 
-print (main_address)
+print ("Bitcoin Address: " + main_address)
 
 
+#you can validate the addresses here: http://lenschulwitz.com/base58
+#https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
+#https://en.bitcoin.it/wiki/Address
